04bb611aa4b145df25c979bbf5d7f05c91c95aba
Your Name <hsssseeeen2002@gmail.com>
2024-06-26 21:29:51 +0300

    deleted some files

--- src/app/profile/Btton.tsx
+++
@@ -1,6 +0,0 @@
-'use client'
-import { signOut } from 'next-auth/react'
-
-export default function Btton() {
-  return <button className="p-2 rounded bg-red-800 text-white hover:text-slate-300 mt-6 w-fit ml-auto" onClick={() => signOut()}>Sign Out</button>
-}
